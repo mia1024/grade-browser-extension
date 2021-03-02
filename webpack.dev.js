a28e66a9f07841e5f77a0ed19d2488f19a0e28d2
Mia Celeste <mia@miaceleste.dev>
2021-03-01 21:51:25 -0800

    Improved styles before moving to Vue

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,21 @@
 const common = require("./webpack.common.js")
 const {merge} = require("webpack-merge")
 const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin")
 
-module.exports=merge(common,{
-    mode:"development",
+module.exports = merge(common, {
+    mode: "development",
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: false,
         port: 9000,
     },
+    plugins: [
+        new CopyPlugin({
+                patterns: [
+                    {from: "src/manifest.json", to: path.resolve(__dirname, "dist", "manifest.json")}
+                ]
+            }
+        ),
+    ]
 })
--- webpack.dist.js
+++ webpack.dist.js
@@ -2,31 +2,37 @@ const CopyPlugin = require("copy-webpack-plugin");
 const ZipPlugin = require('zip-webpack-plugin');
 const common = require("./webpack.common.js")
 const {merge} = require("webpack-merge")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const path = require('path');
 const webpack = require("webpack")
 const fs = require("fs")
 
 
-class DeleteEverythingButZip{
+class DeleteEverythingButZip {
     apply(compiler) {
         compiler.hooks.done.tap(
             "DeleteEverythingButZip", (stats) => {
-                fs.readdirSync("dist").filter(name=>name!=='grade.zip').map(
-                    name=>fs.unlinkSync(path.resolve(__dirname,'dist',name))
+                fs.readdirSync("dist").filter(name => name !== 'grade.zip').map(
+                    name => fs.unlinkSync(path.resolve(__dirname, 'dist', name))
                 )
             }
         )
     }
 }
 
-module.exports=merge(common,{
-    mode:"production",
-    plugins:[
+module.exports = merge(common, {
+    mode: "production",
+    plugins: [
+        new CopyPlugin({
+                patterns: [
+                    {from: "src/dist_manifest.json", to: path.resolve(__dirname, "dist", "manifest.json")}
+                ]
+            }
+        ),
         new ZipPlugin({
-                filename:"grade.zip"
-            }),
-        new CleanWebpackPlugin( {}),
+            filename: "grade.zip"
+        }),
+        new CleanWebpackPlugin({}),
         new DeleteEverythingButZip(),
     ],
     devtool: 'cheap-module-source-map',
